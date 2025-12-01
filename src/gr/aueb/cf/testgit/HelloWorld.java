4624be7b08a581625faee7c4b49a4d203b942f41
Angelos Lavdas <anlavd@yahoo.gr>
2025-12-01 21:02:42 +0200

    feat: modify hello world

--- src/gr/aueb/cf/testgit/HelloWorld.java
+++ src/gr/aueb/cf/testgit/HelloWorld.java
@@ -2,7 +2,7 @@ package gr.aueb.cf.testgit;
 
 public class HelloWorld {
 
-     public static void main() {
-         System.out.println("Hello World !");
+     public static void main(String[] args) {
+         System.out.println("Hello World !" );
      }
 }
